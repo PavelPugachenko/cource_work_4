3b535e2701c33b80ba918a999d2d443fa3585ee5
PavelPugachenko <pugachenkopavel72@gmail.com>
2024-06-24 15:27:47 +0500

    11111

--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -1,7 +1,5 @@
 class FromVacancy:
 
-    """Класс для работы с вакансиями"""
-
     def __init__(self, name, salary, currency, url, requirement='Информация отсутствует'):
         self.name = name
         self.salary = salary
@@ -15,29 +13,8 @@ class FromVacancy:
                 f'Требования: {self.requirement}\n'
                 f'Ссылка на вакансию: <{self.url}>\n')
 
-    def _validate_salary(self, salary):
-        if isinstance(salary, dict) and 'to' in salary:
-            return True
-        return False
-
     def __gt__(self, other):
-
-        """Метод сравнения вакансий между собой по зарплате и валидации данных по зарплате"""
-
-        if self.salary is not None and other.salary is not None:
-            if self.salary['to'] > other.salary['to']:
-                return self
-            else:
-                return other
-        return 'Зарплата не указана'
+        return self.salary['to'] > other.salary['to']
 
     def __lt__(self, other):
-
-        """Метод сравнения вакансий между собой по зарплате и валидации данных по зарплате"""
-
-        if self.salary is not None and other.salary is not None:
-            if self.salary['to'] < other.salary['to']:
-                return self
-            else:
-                return other
-        return 'Зарплата не указана'
+        return self.salary['to'] < other.salary['to']
